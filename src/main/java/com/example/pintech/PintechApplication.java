79108723b178795817664ea7f63f3ca71eb754b6
tjfwltn <tjfwltn1234@gmail.com>
2024-11-27 22:09:12 +0900

    md 작성 및 초기 프로젝트 설정

---
+++ src/main/java/com/example/pintech/PintechApplication.java
@@ -0,0 +1,13 @@
+package com.example.pintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PintechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PintechApplication.class, args);
+    }
+
+}
